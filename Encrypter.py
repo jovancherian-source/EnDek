8bd31ae3f3b617129787c90bb329292f771b52bc
Jovan Cherian <jovancherian@gmail.com>
2026-07-03 14:42:17 +0530

    allow new  clients to add user

--- Encrypter.py
+++ Encrypter.py
@@ -7,9 +7,16 @@ from database_to_dict import database_to_dict
 logos()
 input_username = input("username: ")
 
-users = ["jovancherian"]
-password = '3e3e3e'
 
+connection1 = sqlite3.connect("users.db")
+cursor1 = connection1.cursor()
+cursor1.execute("""
+CREATE TABLE IF NOT EXISTS users(
+            username TEXT PRIMARY KEY,
+            password TEXT)
+""")
+cursor1.execute("SELECT * FROM users ")
+users = database_to_dict(cursor1.fetchall())
 
 
 
@@ -17,7 +24,7 @@ password = '3e3e3e'
 
 if input_username in users:
     input_password_1 = getpass.getpass("sudo: ")
-    if input_password_1 == password:
+    if input_password_1 == users[input_username]:
         connection = sqlite3.connect("encyption_keys.db")
         cursor = connection.cursor()
         cursor.execute("""
@@ -101,86 +108,98 @@ if input_username in users:
                     encrypt1 = database_to_dict(encrypt_demo)
                     Decrypter  = {value: key for key, value in encrypt1.items()}
                     connection.commit()
-
-
-            if user_covert_input[-1] == "E" and user_input !="config":
-                cursor.execute("SELECT * FROM encryptionkeys ")
-                encrypt_demo = cursor.fetchall()
-                encrypt1 = database_to_dict(encrypt_demo)
-                Decrypter  = {value: key for key, value in encrypt1.items()}
-                for i in user_covert_input:
-                    if i in Decrypter:
-                        return_list.append(Decrypter.get(i , "letter not found :("))
-                return_word = "". join(return_list)
-                print(return_word)
-            if user_covert_input[-1] != "E" and user_input !="config":
-                for i in user_covert_input :
-                    try:
-                        return_list.append(encrypt1[i])               
-                        if len(user_covert_input) == len(return_list):
-                            return_list.append("E")
-                            return_sentence = "". join(return_list)
-                            print(return_sentence)
-                    except KeyError:
-                        print("invalid characters")
-    elif input_password_1 != password:
-        print("wrong password!!")              
+                elif user_request == "user":
+                    command = input("are you sure you want to delete your user account(y/n): ")
+                    if command == "y":
+                        cursor1.execute("DELETE FROM users WHERE username = (?)", (input_username,))
+                        connection1.commit()
+            if len(user_covert_input) !=0 :
+                if user_covert_input[-1] == "E" and user_input !="config":
+                    cursor1.execute("SELECT * FROM encryptionkeys ")
+                    encrypt_demo = cursor1.fetchall()
+                    encrypt1 = database_to_dict(encrypt_demo)
+                    Decrypter  = {value: key for key, value in encrypt1.items()}
+                    for i in user_covert_input:
+                        if i in Decrypter:
+                            return_list.append(Decrypter.get(i , "letter not found :("))
+                    return_word = "". join(return_list)
+                    print(return_word)
+                if user_covert_input[-1] != "E" and user_input !="config":
+                    for i in user_covert_input :
+                        try:
+                            return_list.append(encrypt1[i])               
+                            if len(user_covert_input) == len(return_list):
+                                return_list.append("E")
+                                return_sentence = "". join(return_list)
+                                print(return_sentence)
+                        except KeyError:
+                            print("invalid characters")
+            elif input_password_1 != users.get(input_username):
+                print("wrong password!!")
 elif input_username not in users:
-     user_reponse = input("user not found.Do you have an encryption key(y/n): ")
-     if user_reponse == "y":
-        key = input("Key: ")
-        unknown_user_two_d  = list_maker(key)
-        encrypt1 = database_to_dict(unknown_user_two_d)        
-        x = 0
-        while  x < 5:
-                x += 1
-                user_input = input('> ')
-                user_covert_input = list(user_input)
-                return_list = []
-                if user_input == "exit":
-                     break
-                for i in user_covert_input:
-                    try:
-                        return_list.append(encrypt1[i])               
-                        return_sentence = "" . join(return_list)
-                        if len(user_covert_input) == len(list(return_sentence)):
-                            return_list.append("E")
-                            return_sentence = "".join(return_list)                         
-                            print(return_sentence)
-                    except KeyError:
-                        if user_covert_input[-1] == "E":
-                            print("you cannot decrypt without a username")
-                        else:
-                            print("invalid characters")
-     elif user_reponse == "n":
-        user_request= input("would you like to generate Encryption key(y/n): ")
-        if user_request == "y":
-            key = randomgenerator()
-            two_dimentional = list_maker(key)
-            dict_1 = database_to_dict(two_dimentional)
-            print(key)
-            print("command successful...")
-            x =0 
-            while x <6:
-                x += 1
-                user_input = input('> ')
-                user_covert_input = list(user_input)
-                return_list = []
-                if user_input == "exit":
-                     break
-                for i in user_covert_input:
-                    try:
-                        return_list.append(dict_1[i])               
-                        return_sentence = "" . join(return_list)
-                        if len(user_covert_input) == len(list(return_sentence)):
-                            return_list.append("E")
-                            return_sentence = "".join(return_list) 
-                            print(return_sentence)
-                    except KeyError:
-                        if user_covert_input[-1] == "E":
-                            print("you cannot decrypt without a username")
-                        else:
-                            print("invalid characters")
+    new_user = input("user not found. would you like to create a new user(y/n): ")
+    if new_user == "y":
+        new_user_password = getpass.getpass("password: ")
+        cursor1.execute("INSERT INTO users(username, password) VALUES(?,?)", (input_username, new_user_password))
+        connection1.commit()
+        print("user created successfully...")
+
+    elif new_user == "n":
+        user_reponse = input("Do you have an encryption key(y/n): ")
+        if user_reponse == "y":
+            key = input("Key: ")
+            unknown_user_two_d  = list_maker(key)
+            encrypt1 = database_to_dict(unknown_user_two_d)        
+            x = 0
+            while  x < 5:
+                    x += 1
+                    user_input = input('> ')
+                    user_covert_input = list(user_input)
+                    return_list = []
+                    if user_input == "exit":
+                        break
+                    for i in user_covert_input:
+                        try:
+                            return_list.append(encrypt1[i])               
+                            return_sentence = "" . join(return_list)
+                            if len(user_covert_input) == len(list(return_sentence)):
+                                return_list.append("E")
+                                return_sentence = "".join(return_list)                         
+                                print(return_sentence)
+                        except KeyError:
+                            if user_covert_input[-1] == "E":
+                                print("you cannot decrypt without a username")
+                            else:
+                                print("invalid characters")
+        elif user_reponse == "n":
+            user_request= input("would you like to generate Encryption key(y/n): ")
+            if user_request == "y":
+                key = randomgenerator()
+                two_dimentional = list_maker(key)
+                dict_1 = database_to_dict(two_dimentional)
+                print(key)
+                print("command successful...")
+                x =0 
+                while x <6:
+                    x += 1
+                    user_input = input('> ')
+                    user_covert_input = list(user_input)
+                    return_list = []
+                    if user_input == "exit":
+                        break
+                    for i in user_covert_input:
+                        try:
+                            return_list.append(dict_1[i])               
+                            return_sentence = "" . join(return_list)
+                            if len(user_covert_input) == len(list(return_sentence)):
+                                return_list.append("E")
+                                return_sentence = "".join(return_list) 
+                                print(return_sentence)
+                        except KeyError:
+                            if user_covert_input[-1] == "E":
+                                print("you cannot decrypt without a username")
+                            else:
+                                print("invalid characters")
 
 
          
