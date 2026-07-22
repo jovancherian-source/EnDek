2034d1d7b7959b157f35f50a8c9678b0a23e96ca
Jovan Cherian <jovancherian@gmail.com>
2026-07-22 15:46:08 +0530

    Update twod_list_maker.py

--- twod_list_maker.py
+++ twod_list_maker.py
@@ -2,7 +2,7 @@ def list_maker(input_string):
     one_d_list = list(input_string)
     two_d_list = []
     for item in range(0 ,len(one_d_list), 2):
-        if item != len(one_d_list)-1:
+        
             two_d_list.append([one_d_list[item], one_d_list[item+1]])
 
 
